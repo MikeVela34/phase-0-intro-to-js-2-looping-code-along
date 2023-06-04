fb2ff0899a227b8cb0c08f95091ef9a5c9a4dcf0
Michael Vela <mvelajr52@gmail.com>
2023-06-04 04:10:50 -0500

    pass test

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+/*const gifts = ["teddy bear", "drone", "doll"]
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bowl`);
+        debugger;
+    }
+
+    return gifts;
+}
+wrapGifts(gifts);
+*/
+
+function writeCards(namesArray, event) {
+    const cards = []
+    for (let i = 0; i < namesArray.length; i++) {
+        cards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return cards;
+}
+
+function countDown(num) {
+    while (num > -1) {
+        console.log(num--);
+    }
+}
